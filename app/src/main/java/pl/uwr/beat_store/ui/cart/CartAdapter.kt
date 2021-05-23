d66c47838758c9cde1c69f7d9ab8283fe554542c
mbozecki <mboz@interia.pl>
2021-05-23 21:25:35 +0200

    CartAdapter+CartFragment+recyclerview

---
+++ app/src/main/java/pl/uwr/beat_store/ui/cart/CartAdapter.kt
@@ -0,0 +1,44 @@
+package pl.uwr.beat_store.ui.cart
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import pl.uwr.beat_store.R
+import pl.uwr.beat_store.data.models.Song
+
+class CartAdapter(
+        private var songs: ArrayList<Song>,
+        private var context: Context
+) : RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
+
+    private lateinit var song: Song;
+    private var layoutinflater: LayoutInflater= LayoutInflater.from(context);
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
+        var view: View = layoutinflater.inflate(R.layout.single_cart_element, parent, false)
+        return CartViewHolder(view);
+    }
+
+    override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
+        song= songs[position];
+        holder.songName.text = song.name;
+        holder.producerName.text= song.producer;
+        Picasso.get().load(song.image).into(holder.songImage);
+    }
+
+    override fun getItemCount(): Int {
+        return songs.size;
+    }
+
+    class CartViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var songImage: ImageView = view.findViewById(R.id.cart_beatImage);
+        var songName: TextView = view.findViewById(R.id.cart_beatName);
+        var producerName: TextView = view.findViewById(R.id.cart_producerName);
+
+    }
+}
--- app/src/main/java/pl/uwr/beat_store/ui/cart/CartFragment.kt
+++ app/src/main/java/pl/uwr/beat_store/ui/cart/CartFragment.kt
@@ -7,17 +7,36 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import pl.uwr.beat_store.R
+import pl.uwr.beat_store.data.models.Song
+import pl.uwr.beat_store.ui.cart.CartAdapter
 
 class CartFragment : Fragment() {
 
     private lateinit var cartViewModel: CartViewModel
+    private var layoutManager: RecyclerView.LayoutManager? = null;
+    private var adapter: RecyclerView.Adapter<CartAdapter.CartViewHolder>? = null;
+    private var songList= ArrayList<Song>();
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState);
+        var rvCart: RecyclerView = view.findViewById(R.id.rvCart);
+        layoutManager= LinearLayoutManager(requireContext());
+        adapter= CartAdapter(songList, requireContext());
+        rvCart.layoutManager= layoutManager;
+        rvCart.adapter= adapter;
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        songList.add(Song("url","Nazwa","Producento", "https://cdn.beatstars.com/eyJidWNrZXQiOiJidHMtY29udGVudCIsImtleSI6InVzZXJzL3Byb2QvMjAxMDIxL2ltYWdlL3YxTDhjVllabHVYSC9pNGIzeHNydWkuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7ImZpdCI6bnVsbCwid2lkdGgiOjIwMCwiaGVpZ2h0IjoyMDB9fX0=","weeknd" ))
+        songList.add(Song("url","Nazwa","Producento", "https://cdn.beatstars.com/eyJidWNrZXQiOiJidHMtY29udGVudCIsImtleSI6InVzZXJzL3Byb2QvMjAxMDIxL2ltYWdlL3YxTDhjVllabHVYSC9pNGIzeHNydWkuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7ImZpdCI6bnVsbCwid2lkdGgiOjIwMCwiaGVpZ2h0IjoyMDB9fX0=","weeknd" ))
+
         cartViewModel =
                 ViewModelProvider(this).get(CartViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_cart, container, false)
